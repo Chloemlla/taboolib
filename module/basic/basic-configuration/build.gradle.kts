2eb93b573802c3e478460df6661b5ce26e3bf511
黑 <b@taboo.sh>
2025-08-25 18:46:09 +0800

    build: 更新 Reflex 依赖版本至 1.2.0

--- module/basic/basic-configuration/build.gradle.kts
+++ module/basic/basic-configuration/build.gradle.kts
@@ -16,6 +16,16 @@ dependencies {
     compileOnly("com.electronwill.night-config:json:3.6.7")
     compileOnly("com.electronwill.night-config:hocon:3.6.7")
     implementation("com.electronwill.night-config:core-conversion:6.0.0")
+
+    // 测试
+    testImplementation(project(":common-util"))
+    testImplementation("org.yaml:snakeyaml:2.2")
+    testImplementation("com.typesafe:config:1.4.3")
+    testImplementation("com.electronwill.night-config:core:3.6.7")
+    testImplementation("com.electronwill.night-config:toml:3.6.7")
+    testImplementation("com.electronwill.night-config:json:3.6.7")
+    testImplementation("com.electronwill.night-config:hocon:3.6.7")
+    testImplementation("com.electronwill.night-config:core-conversion:6.0.0")
 }
 repositories {
     mavenCentral()
