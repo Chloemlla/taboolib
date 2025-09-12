153db73bb31afd16ce3a07b9556cf4c56cf9215d
黑 <b@taboo.sh>
2025-09-12 22:39:31 +0800

    写错了

--- module/minecraft/minecraft-kether/src/main/java/taboolib/library/kether/BlockReader.java
+++ module/minecraft/minecraft-kether/src/main/java/taboolib/library/kether/BlockReader.java
@@ -109,7 +109,7 @@ public class BlockReader extends AbstractStringReader {
             Quest.Block block = new SimpleQuest.SimpleBlock(name, actions);
             this.blocks.put(block.getLabel(), block);
             head.set(ActionProperties.BLOCK, block.getLabel());
-            return actions.getLast();
+            return head;
         } else {
             return ParsedAction.noop();
         }
